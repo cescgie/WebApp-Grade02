28e87e4bfe11852c51f17269b833582ab067cfad
Yoggi Firmanda <yoggifirmanda@gmail.com>
2018-01-07 02:25:58 +0100

    Change json params strict with underline

--- backend/app/commands/import_result.py
+++ backend/app/commands/import_result.py
@@ -2,6 +2,9 @@ from flask import current_app
 from flask_script import Command
 
 from app import db
+import json
+
+from app.models.result_models import Result
 
 class ImportResultCommand(Command):
     """ Initialize the database."""
@@ -12,4 +15,10 @@ class ImportResultCommand(Command):
 def import_result():
     """ Import RESULT data to the database."""
     print('Import RESULT')
+    with open('btw17_kerg.json') as json_data:
+        daten = json.load(json_data)
+        for data in daten:
+            # Check if data with nr = data['nr'] already inserted
+            # If true, skip
+            # If false, insert to table results
     

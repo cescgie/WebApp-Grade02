ec026a78799234a40bdb9686a1c4660d5506deda
Yoggi Firmanda <yoggifirmanda@gmail.com>
2018-01-08 22:47:07 +0100

    Api for total result and from each bundesland

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,11 +6,35 @@ cors = CORS(api_blueprint, resources={r"/api/*": {"origins": "*"}})
 
 from app.models.result_models import Result
 
-# The Home page is accessible to anyone
-@api_blueprint.route('/api/json')
-def json():
-    results = Result.query.order_by(Result.id).all()
-    for result in results:
-        print(result.gebiet)
-    jsonData = '{"name": "Max Mustermann", "hometown": {"name": "Berlin", "id": 123}}'
-    return jsonData
+from flask_jsonpify import jsonify
+
+nr = []
+for x in range(501, 517):
+    nr.append(x)
+    
+# Format to valid result
+def formatResult(dataRow):
+    data = {}
+    for key in dataRow.keys():
+        if(key != '_sa_instance_state'):
+            data[key] = dataRow[key]
+    return data
+
+# Get total result        
+@api_blueprint.route('/api/result/all')
+def getAllResult():
+    result = Result.query.filter(Result.nr == 500).first()
+    dicti = result.__dict__
+    data = formatResult(dicti)
+    return jsonify(data)
+
+# Get result from each bundesland
+@api_blueprint.route('/api/result/bundesland')
+def getBundesland():
+    land = []
+    for x in nr:
+        result = Result.query.filter(Result.nr == x).first()
+        dicti = result.__dict__
+        data = formatResult(dicti)
+        land.append(data)
+    return jsonify(land)
